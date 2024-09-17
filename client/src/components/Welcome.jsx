fe5ecdd33b54560857ddfd3a84d31f889842bec2
NoushinP <noushinazari89@gmail.com>
2024-09-17 11:46:24 -0500

    added count in localstorage

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,12 +1,29 @@
 import { useQuery } from '@apollo/client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import "./Welcome.css"
 import { QUERY_QUESTION } from '../utils/queries'
 import QuestionDisplay from './QuestionDisplay'
 
 const Welcome = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [clickCounts, setClickCounts] = useState({});
     const { loading, data } = useQuery(QUERY_QUESTION);
+
+    useEffect(() => {
+        const savedClickCounts = localStorage.getItem('clickCounts');
+        if (savedClickCounts) {
+            setClickCounts(JSON.parse(savedClickCounts));
+        }
+    }, []);
+
+    useEffect(() => {
+        if (Object.keys(clickCounts).length > 0) {
+        localStorage.setItem('clickCounts', JSON.stringify(clickCounts));
+        }
+    }, [clickCounts]);
+   
+
+
 
     if (loading) {
         return <div>Loading...</div>;
@@ -14,9 +31,31 @@ const Welcome = () => {
 
     const questions = data?.questions || [];
 
+    if (questions.length === 0) {
+        return <div>No questions available</div>;
+    }
+
+    const handleChoiceClick = (choice) => {
+        const questionId = questions[currentQuestionIndex]?.id;
+        
+    
+        const updatedCounts = {
+            ...clickCounts,
+            [questionId]: {
+                ...clickCounts[questionId],
+                [choice]: (clickCounts[questionId]?.[choice] || 0) + 1,
+            },
+        };
+        
+        setClickCounts(updatedCounts); 
+    };
+
+
+
     const handleNextClick = () => {
         if (currentQuestionIndex < questions.length - 1) {
             setCurrentQuestionIndex(currentQuestionIndex + 1);
+            console.log(clickCounts)
         }
     }
 
@@ -26,6 +65,7 @@ const Welcome = () => {
             <h2>😱Discover what people really think!😱</h2>
             <QuestionDisplay 
                 question={questions[currentQuestionIndex]} 
+                onChoiceClick={handleChoiceClick} 
             />
             {currentQuestionIndex < questions.length - 1 && (
                 <button onClick={handleNextClick}>Next</button>
