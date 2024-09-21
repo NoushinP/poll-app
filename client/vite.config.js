202c1867828ff89f07e54f01e2a6cf48ddd79073
Neil <nsducklow@gmail.com>
2024-09-21 09:15:06 -0500

    display issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const ASSET_URL = process.env.ASSET_URL || '';
+// const ASSET_URL = process.env.ASSET_URL || '';
 export default defineConfig({
-  base: `${ASSET_URL}/dist/`,
+  // base: `${ASSET_URL}/dist/`,
   plugins: [react()],
   server: {
-    port: 3000,
+    host: true,
+    strictPort: true,
+    port: 8000,
     open: true,
     proxy: {
       '/graphql': {
