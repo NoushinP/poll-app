b1cfb61de89aee7220da4c129b67dd59169b3c06
Neil <nsducklow@gmail.com>
2024-09-21 10:57:38 -0500

    update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// const ASSET_URL = process.env.ASSET_URL || '';
-// console.log(ASSET_URL)
+const ASSET_URL = process.env.ASSET_URL || '';
 export default defineConfig({
-  // base: `${ASSET_URL}/dist/`,
+  base: `${ASSET_URL}/client/dist/`,
   plugins: [react()],
   server: {
-    base: '/client/dist',
+    // base: '/client/dist',
     port: 3000,
     open: true,
     proxy: {
