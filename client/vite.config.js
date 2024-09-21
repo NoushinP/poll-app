c2a40740c796b6fc6dbdaa7ab84421307a336a5b
Neil <nsducklow@gmail.com>
2024-09-21 12:45:28 -0500

    regular expression

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         target: ASSET_URL,
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/graphql/, '')
       }, 
     } 
 
